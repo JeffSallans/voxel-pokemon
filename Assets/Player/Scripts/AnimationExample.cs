f8c9807effded189f148b9617259647f22441cc5
Jeff Sallans <jeffsallans@gmail.com>
2021-11-13 00:31:22 -0500

    Walking animation and world map

---
+++ Assets/Player/Scripts/AnimationExample.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Used to demo the walking animation for the character.  Hold W to see it work.
+/// </summary>
+[RequireComponent(typeof(Animator))]
+public class AnimationExample : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Press w to trigger walk animation");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            gameObject.GetComponent<Animator>().SetBool("isWalking", true);
+        }
+        else
+        {
+            gameObject.GetComponent<Animator>().SetBool("isWalking", false);
+        }
+    }
+}
