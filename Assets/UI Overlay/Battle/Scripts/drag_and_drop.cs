609323c0bd9f6ae6d61f4358a5a184711cf1a0d8
DangerChickn <94122322+DangerChickn@users.noreply.github.com>
2021-11-29 21:11:32 -0500

    more test_battle progress

---
+++ Assets/UI Overlay/Battle/Scripts/drag_and_drop.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class drag_and_drop : MonoBehaviour
+{
+    private bool isDragging = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void StartDrag()
+    {
+        isDragging = true;
+    }
+
+    public void EndDrag()
+    {
+        isDragging = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDragging)
+        {
+            transform.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        }
+        
+    }
+}
