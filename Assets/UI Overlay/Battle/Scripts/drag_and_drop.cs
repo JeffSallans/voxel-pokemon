135f8ea59c787009e89275e5a88a8fc6cc1cd952
DangerChickn <94122322+DangerChickn@users.noreply.github.com>
2021-11-30 23:43:17 -0500

    battle progress

--- Assets/UI Overlay/Battle/Scripts/drag_and_drop.cs
+++ Assets/UI Overlay/Battle/Scripts/drag_and_drop.cs
@@ -8,22 +8,52 @@ public class drag_and_drop : MonoBehaviour
     public GameObject handArea;
 
     private bool isDragging = false;
+    private GameObject dropzone;
+    private GameObject dropArea;
+    private GameObject startParent;
+    private Vector2 startPos;
+    private bool isOverDropZone = false;
 
     // Start is called before the first frame update
     void Start()
     {
         canvas = GameObject.Find("Main Canvas");
         handArea = GameObject.Find("Player Hand");
+        dropArea = GameObject.Find("Drop Zone");
     }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        isOverDropZone = true;
+        dropzone = collision.gameObject;
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        isOverDropZone = false;
+        dropzone = null;
+    }
     public void StartDrag()
     {
         isDragging = true;
+        startParent = transform.parent.gameObject;
+        startPos = transform.position;
     }
 
     public void EndDrag()
     {
         isDragging = false;
+        if (isOverDropZone)
+        {
+            transform.position = dropArea.transform.position;
+            transform.SetParent(dropArea.transform, false);
+        }
+        else
+        {
+            transform.position = startPos;
+            transform.SetParent(startParent.transform, false);
+        }
     }
 
     // Update is called once per frame
