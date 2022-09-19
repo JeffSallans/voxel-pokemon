9f8d44d8afdf879025faf2a269f5dbf0f87449fd
Steve Dougherty <steve@asksteved.com>
2022-09-19 04:54:15 -0400

    Tweak walking animation flow; prevent consecutive repeat walking sounds

--- Assets/Player/Scripts/AnimationExample.cs
+++ Assets/Player/Scripts/AnimationExample.cs
@@ -1,39 +1,67 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 
 /// <summary>
-/// Used to demo the walking animation for the character.  Hold W to see it work.
+/// Plays walking animations and sounds for the character.
 /// </summary>
 [RequireComponent(typeof(Animator))]
 public class AnimationExample : MonoBehaviour
 {
 
-    public List<AudioSource> audioSourceList = null;
+    public List<AudioSource> audioSourceList;
+    private int lastPlayed = -1;
 
-    // Start is called before the first frame update
-    void Start()
+    public List<KeyCode> movementKeys = new List<KeyCode>
     {
-        print("Press w to trigger walk animation");
+        KeyCode.W,
+        KeyCode.A,
+        KeyCode.S,
+        KeyCode.D,
+    };
+    private static readonly int IsWalking = Animator.StringToHash("isWalking");
+
+    private Animator _animator;
+    private Animator animator
+    {
+        get
+        {
+            if (_animator == null)
+            {
+                _animator = GetComponent<Animator>();
+            }
+
+            return _animator;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+        // Start (or continue) walking animation when any movement key starts being pressed.
+        if (movementKeys.Any(Input.GetKeyDown))
         {
-            gameObject.GetComponent<Animator>().SetBool("isWalking", true);
-            var isSoundPlaying = audioSourceList.Any(a => a.isPlaying);
-            if (!isSoundPlaying && audioSourceList.Count > 0)
-            {
-                var randomIndex = Random.Range(0, audioSourceList.Count - 1);
-                audioSourceList[randomIndex].Play();
-            }
+            animator.SetBool(IsWalking, true);
         }
-        else
+        // Stop walking animation when any movement key is released and now none are pressed.
+        else if (movementKeys.Any(Input.GetKeyUp) && !movementKeys.Any(Input.GetKey))
         {
-            gameObject.GetComponent<Animator>().SetBool("isWalking", false);
+            animator.SetBool(IsWalking, false);
         }
+
+        // Play a random walking sounds (without consecutive repeats) continuously while walking.
+        if (!animator.GetBool(IsWalking)) return;
+        if (audioSourceList.Any(a => a.isPlaying) || audioSourceList.Count <= 0) return;
+
+        // Pick from a list that doesn't include the unwanted index to avoid
+        // technically-inconsistent/unbounded runtime by re-randomizing when
+        // the unwanted index is randomly picked.
+        var indexes = new List<int>();
+        indexes.AddRange(Enumerable.Range(0, audioSourceList.Count));
+        indexes.Remove(lastPlayed);
+        var randomIndex = indexes[Random.Range(0, indexes.Count)];
+
+        lastPlayed = randomIndex;
+        audioSourceList[randomIndex].Play();
     }
 }
